016f3555521d16715776dd75d29da3f16252faea
Graham Campbell <graham@mineuk.com>
2015-01-21 22:28:27 +0000

    Updated the license information

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -1,15 +1,12 @@
 <?php
 
 /*
- * This file is part of Starbs Http by Graham Campbell.
+ * This file is part of Starbs Http.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Starbs\Http\Controllers;
@@ -18,6 +15,11 @@ use Orno\Di\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * This is the abstract controller class.
+ *
+ * @author Graham Campbell <graham@mineuk.com>
+ */
 abstract class AbstractController
 {
     /**
